45c90bb6d6cab7a896b75b430413bc3914658682
mozzam inamdar <mozzam607@gmail.com>
2023-09-11 22:47:55 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+const mongoose = require("mongoose");
+const app = require("./app");
 require("dotenv").config();
 
-const mongoose = require("mongoose");
-const app = require("./app")
 
-
-
+// Environment variables
 const PORT = process.env.PORT || 8000;
 const DB = process.env.DATABASE;
+
 
 app.listen(PORT, () => {
   console.log("Listening on Port: ", PORT);
 });
 
 
+// Database connection
 mongoose
   .connect(DB)
   .then(() => {
