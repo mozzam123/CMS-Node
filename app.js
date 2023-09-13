07f8736ff230eb50ec987ce9a05b8db9f0929138
mozzam inamdar <mozzam607@gmail.com>
2023-09-13 16:10:34 +0530

    worked on getcontent route

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 const userRouter = require("./routes/userRoutes");
+const contentRouter = require("./routes/contentRoutes");
 
 // Middlewares
 app.use(express.json());
 
 app.use(userRouter);
+app.use(contentRouter);
 
 module.exports = app;
--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,3 +1,24 @@
-const contentModel = require("./../models/contentModel")
+const contentModel = require("./../models/contentModel");
+const userModel = require("./../models/userModel");
 
+exports.getContent = async (req, res) => {
+  const username = req.body.username;
 
+  const existingUser = await userModel.findOne({ email: username });
+
+  try {
+    if (existingUser) {
+        // console.log(existingUser);
+      if (existingUser.role === "author") {
+        console.log("*******role is author");
+        const authorContent = await contentModel.findOne({ email: username})
+        return res.json({ message: authorContent });
+      } else if (existingUser.role === "admin") {
+        console.log("*******role is admin");
+      }
+    }
+    res.json({ message: "in the getcontent" });
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
